a5c747beb7b5d4a9d8e610a739cdebd677bfbd96
yulisartika <yulisrtk@gmail.com>
2021-02-11 22:12:27 +0700

    adding login teacher full layout

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,99 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Jumbotron } from "reactstrap";
 
 function Login() {
   return (
     <div className="login-page">
       <div className="login-form">
         <div className="login-header">
-          <div
-            style={{
-              fontSize: "24px",
-              fontWeight: "bold",
-            }}
-          >
-            Welcome Back!
-          </div>
-          <div
-            style={{
-              fontSize: "18px",
-              fontWeight: "400",
-            }}
-          >
-            Login to your account
-          </div>
+          <div className="login-welcome">Welcome Back!</div>
+          <div className="login-yourAccount">Login to your account</div>
         </div>
-        <div>
-          <p
-            style={{
-              fontWeight: "bold",
-              marginBottom: "7px",
-            }}
-          >
-            Email*
+        <form>
+          <p className="email">
+            Email<span>*</span>
           </p>
-          <div
-            style={{
-              borderBottom: "2px grey solid",
-              paddingTop: "8px",
-              paddingBottom: "2px",
-              background: "whitesmoke",
-            }}
-          >
+          <div className="email-password-field">
             <input
-              type="text"
+              type="email"
               placeholder="john@doe.com"
               className="login-email"
             />
           </div>
-          <p
-            style={{
-              fontWeight: "bold",
-              marginBottom: "7px",
-            }}
-          >
-            Password*
+          <p className="password">
+            Password<span>*</span>
           </p>
-          <div
-            style={{
-              borderBottom: "2px grey solid",
-              paddingTop: "8px",
-              paddingBottom: "2px",
-              background: "whitesmoke",
-            }}
-          >
+          <div className="email-password-field">
             <input
               type="password"
               placeholder="********"
               className="login-password"
             />
           </div>
-          <div
-            style={{
-              textAlign: "right",
-              padding: "5px 0px 25px 0px",
-            }}
-          >
-            Forgot Password
-          </div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              paddingBottom: "15px",
-            }}
-          >
+          <div className="forget-pass">Forgot Password</div>
+          <div className="btn">
             <button type="submit" className="login-button">
               Login
             </button>
           </div>
           <div className="register-redirect">
-            New user?{" "}
-            <Link to="#" style={{ textDecoration: "none", color: "blue" }}>
-              Create an account
-            </Link>
+            New user? <Link to="#">Create an account</Link>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
