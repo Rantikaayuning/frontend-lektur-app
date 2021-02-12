e7575e06f306ba138fb9d54af21b5e3f6296089e
rantika <rantika97@gmail.com>
2021-02-12 07:05:54 +0700

    merge feature/login to develop

--- src/components/Header/SideBar.js
+++ src/components/Header/SideBar.js
@@ -4,50 +4,42 @@ import {Link} from "react-router-dom"
 import garis from "../../assets/Rectangle 2.png";
 
 function SideBar() {
-    return ( <
-        div className = "sidebar fixed" >
-        <
-        div className = "left" >
-        <Link to="/"
-        className = "logo" >
-        <
-        img src = { Logo }
-        alt = "logo"
-        className = "bl" / >
-        <
-        /Link>{" "} <
-        img src = { garis }
-        alt = "garis"
-        className = "bl" / >
-        <
-        /div>{" "} <
-        div className = "center" >
-        <
-        input type = "text"
-        placeholder = "Search Course or Lecturer" / >
-        <
-        i className = "fa fa-search icon" > < /i>{" "} <
-        /div>{" "} <
-        div className = "right" >
-        <
-        ul >
-        <
-        li > Category < /li> <li> For Teacher </li >
-        <
-        li >
-        <
-        div className = "vl" > < /div>{" "} <
-        /li>{" "} <
-        li > { " " } <
-        span > Login < /span>{" "} <
-        /li>{" "} <
-        li >
-        <Link to="/register"><
-        button > Sign Up < /button></Link>{" "} <
-        /li>{" "} <
-        /ul>{" "} <
-        /div>{" "} <
-        /div>
+    return ( 
+        <div className = "sidebar fixed" >
+            <div className = "left" >
+                <a href = "/" className = "logo" >
+                    <img src = { Logo } alt = "logo" className = "bl"/>
+                </a>{" "} 
+                    <img src = { garis } alt = "garis" className = "bl" />
+            </div>{" "} 
+            <div className = "center" >
+                <input type = "text" placeholder = "Search Course or Lecturer"/>
+                <i className = "fa fa-search icon" ></i>{" "} 
+            </div>{" "} 
+            <div className = "right" >
+                <ul >
+                    <li > 
+                        Category 
+                    </li> 
+                    <li> 
+                        For Teacher 
+                    </li >
+                    <li >
+                        <div className = "vl" ></div>{" "}
+                    </li>{" "} 
+                    <li className='sidebar-login-button'> { " " }
+                        <Link to='/login'>
+                            <button > Login </button>{" "} 
+                        </Link>
+                    </li>{" "} 
+                    <li className='sidebar-signup-button'>
+                        <Link to='/register'>
+                            <button>Sign Up </button>{" "} 
+                        </Link>
+                    </li>{" "} 
+                </ul>{" "} 
+            </div>{" "}
+        </div>
     );
 }
 
