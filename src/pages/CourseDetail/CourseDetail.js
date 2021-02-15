f86f111bfaeb7e20988d9b016709075146d1421d
Abdurrohim_ethan <abdurrohim48@gmail.com>
2021-02-15 16:15:37 +0700

    adding related course detail

--- src/pages/CourseDetail/CourseDetail.js
+++ src/pages/CourseDetail/CourseDetail.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { Row, Col } from "reactstrap";
+
+// import { Link } from 'react-router-dom'
+import ContentCards from "../../components/ContentCard/Cards";
+import { cardMaterial } from "../../assets/JSONFile/dummyData";
 
 function CourseDetail() {
   return (
@@ -10,6 +15,24 @@ function CourseDetail() {
         <button> Enroll Now </button>
       </div>
       <div className="course-detail-right"></div>
+      <div className="card-content">
+        <div className="card-text-course">Related Course</div>
+        <Row className="content-card-container">
+          {cardMaterial.map((item, index) => (
+            <Col xs="6" sm="6" md="3" key={index} className="card-container">
+              <ContentCards
+                image={item.image}
+                text={item.text}
+                title={item.title}
+                lecture={item.lecture}
+                video_numbers={item.video_numbers}
+                material_numbers={item.material_numbers}
+                footer={item.footer}
+              />
+            </Col>
+          ))}
+        </Row>
+      </div>
     </div>
   );
 }
