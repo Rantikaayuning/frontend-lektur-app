b5744b630c0ec793549d0bcd1e90430a2c66b152
rantika <rantika97@gmail.com>
2021-02-16 05:15:03 +0700

    revise error line on app.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./styles.css";
 
 //Components
-// import SideBarTeacher from "./components/Header/SideBarTeacher";
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer";
 import HomePage from './pages/HomePage/index'
 import SignUpStudent from "./components/Form/SignUpStudent"
-import SignUpTeacher from "./components/Form/SignUpTeacher
+import SignUpTeacher from "./components/Form/SignUpTeacher"
 import Login from "./components/Form/Login";
 import StudentBoardCourses from "./pages/StudentDashboard/Tab/Courses";
 import StudentMaterial from "./pages/StudentMaterial";
@@ -27,18 +26,18 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/register-student' component={SignUpStudent}/>
-        <Route exact path='/register-teacher' component={SignUpTeacher}/>
-        <Route exact path='/login' component={Login}/>
+        <Route path='/register-student' component={SignUpStudent}/>
+        <Route path='/register-teacher' component={SignUpTeacher}/>
+        <Route path='/login' component={Login}/>
         <Route path='/student-courses' component={StudentBoardCourses}/>
         <Route path='/student-assessment' component={StudentBoardAssessment}/>
         <Route path='/material' component={StudentMaterial}/>
         <Route path='/assessment' component={StudentAssessment} />
         <Route path='/teacher-dashboard' component={TeacherDashboard} />
-        <Route exact path='/teacher-new-course' component={TeacherCourseTab} />
-        <Route exact path='/teacher-new-assessment' component={TeacherAssessmentTab} />
-        <Route exact path='/teacher-new-students' component={TeacherStudentsTab} />
-        <Route exact path="/course-detail" component={CourseDetail} />
+        <Route path='/teacher-new-course' component={TeacherCourseTab} />
+        <Route path='/teacher-new-assessment' component={TeacherAssessmentTab} />
+        <Route path='/teacher-new-students' component={TeacherStudentsTab} />
+        <Route path="/course-detail" component={CourseDetail} />
       </Switch>   
       <Footer />
     </BrowserRouter>
