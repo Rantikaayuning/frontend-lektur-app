2edb4cbf81b25c7d393bcb83105a78957bdedbb1
Masagus Hariadi Arief <adi.arief77@gmail.com>
2021-02-16 10:09:15 +0700

    refactore: register function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import "./styles.css";
 
 //Components
 // import SideBarTeacher from "./components/Header/SideBarTeacher";
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
 import Footer from "./components/Footer";
-import HomePage from './pages/HomePage/index'
-import SignUpStudent from "./components/Form/SignUpStudent"
-import SignUpTeacher from "./components/Form/SignUpTeacher"
+import HomePage from "./pages/HomePage/index";
+import SignUp from "./components/Form/SignUp";
 import Login from "./components/Form/Login";
-import StudentBoardCourses from "./pages/StudentDashboard/Tab/Courses"
-import StudentMaterial from "./pages/StudentMaterial"
+import StudentBoardCourses from "./pages/StudentDashboard/Tab/Courses";
+import StudentMaterial from "./pages/StudentMaterial";
 import TeacherDashboard from "./pages/TeacherDashboard";
 import StudentAssessment from "./pages/StudentAssessment";
 import TeacherCourseTab from "./pages/TeacherNewCourse/Tab/Course";
 import TeacherAssessmentTab from "./pages/TeacherNewCourse/Tab/Assessment";
-import TeacherStudentsTab from "./pages/TeacherNewCourse/Tab/Students"
+import TeacherStudentsTab from "./pages/TeacherNewCourse/Tab/Students";
 import StudentBoardAssessment from "./pages/StudentDashboard/Tab/Assessment";
 
 function App() {
@@ -25,19 +24,26 @@ function App() {
     <BrowserRouter>
       <Header />
       <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route exact path='/register-student' component={SignUpStudent}/>
-        <Route exact path='/register-teacher' component={SignUpTeacher}/>
-        <Route exact path='/login' component={Login}/>
-        <Route path='/student-courses' component={StudentBoardCourses}/>
-        <Route path='/student-assessment' component={StudentBoardAssessment}/>
-        <Route path='/material' component={StudentMaterial}/>
-        <Route path='/assessment' component={StudentAssessment} />
-        <Route path='/teacher-dashboard' component={TeacherDashboard} />
-        <Route exact path='/teacher-new-course' component={TeacherCourseTab} />
-        <Route exact path='/teacher-new-assessment' component={TeacherAssessmentTab} />
-        <Route exact path='/teacher-new-students' component={TeacherStudentsTab} />
-      </Switch>   
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/register-teacher" component={SignUp} />
+        <Route exact path="/login" component={Login} />
+        <Route path="/student-courses" component={StudentBoardCourses} />
+        <Route path="/student-assessment" component={StudentBoardAssessment} />
+        <Route path="/material" component={StudentMaterial} />
+        <Route path="/assessment" component={StudentAssessment} />
+        <Route path="/teacher-dashboard" component={TeacherDashboard} />
+        <Route exact path="/teacher-new-course" component={TeacherCourseTab} />
+        <Route
+          exact
+          path="/teacher-new-assessment"
+          component={TeacherAssessmentTab}
+        />
+        <Route
+          exact
+          path="/teacher-new-students"
+          component={TeacherStudentsTab}
+        />
+      </Switch>
       <Footer />
     </BrowserRouter>
   );
--- src/redux/actions/UserAction.js
+++ src/redux/actions/UserAction.js
@@ -1,53 +1,34 @@
-import API from '../../api/index';
-import {LOGIN, SIGNUP_STUDENT, SIGNUP_TEACHER} from "../types/UserLogin";
+import API from "../../api/index";
+import { LOGIN, SIGNUP_STUDENT, SIGNUP_TEACHER } from "../types/UserLogin";
 
 export const postLogin = (body) => (dispatch) => {
-    API.post('/users/login', body)
+  API.post("/users/login", body)
     .then((response) => {
-        if (response.status === 200) {
-          dispatch({
-            type: LOGIN,
-            payload: response.data.msg,
-            token: localStorage.setItem("token", response.data.data.token)
-          });
-        } 
-      })
-      .catch((payload) =>{alert (payload.response.data.msg)})  
-    }
+      if (response.status === 200) {
+        dispatch({
+          type: LOGIN,
+          payload: response.data.msg,
+          token: localStorage.setItem("token", response.data.data.token),
+        });
+      }
+    })
+    .catch((payload) => {
+      alert(payload.response.data.msg);
+    });
+};
 
-
-export const postSignupStudent = (body) => (dispatch) => {
-  API.post("/users/register?status=0", body)
+export const signup = (role, payload) => (dispatch) => {
+  API.post(`/users/register?status=${role}`, payload)
     .then((response) => {
-        if (response.status === 200) {
-          dispatch({
-            type: SIGNUP_STUDENT,
-            payload: response.data.msg,
-          })
-          alert("Sign up is successful, please continue to login")
-        }
-      })
-    .catch((payload) =>{alert (payload.response.data.msg)})
-    }
-  
-    
-export const postSignupTeacher = (body) => (dispatch) => {
-    API.post("/users/register?status=1", body)
-    .then((response) => {
-        if (response.status === 200) {
-          dispatch({
-            type: SIGNUP_TEACHER,
-            payload: response.data.msg,
-          })
-          alert("Sign up is successful, please continue to login")
-        }
-      })
-    .catch((payload) =>{alert (payload.response.data.msg)})
-    }
-  
-  
-  
-  
-  
-  
-
+      if (response.status === 200) {
+        dispatch({
+          type: SIGNUP_TEACHER,
+          payload: response.data.msg,
+        });
+        alert("Sign up is successful, please continue to login");
+      }
+    })
+    .catch((payload) => {
+      alert(payload.response.data.msg);
+    });
+};
