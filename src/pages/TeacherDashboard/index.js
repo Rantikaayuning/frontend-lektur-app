7da86f5183386fae54cc71dcf39b5521fe34c566
yulisartika <yulisrtk@gmail.com>
2021-02-16 08:31:57 +0700

    fixing some styling for login form and teacher dashboard

--- src/pages/TeacherDashboard/index.js
+++ src/pages/TeacherDashboard/index.js
@@ -1,66 +1,84 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import { courseCardJson, teacherProfile } from "../../assets/JSONFile/dummyData";
+import {
+  courseCardJson,
+  teacherProfile,
+} from "../../assets/JSONFile/dummyData";
 import CourseCard from "./CourseCard";
 
 function TeacherDashboard() {
-    const [isEdit, setEdit] = useState(true)
+  const [isEdit, setEdit] = useState(true);
 
-    const handleEdit = () => {
-        setEdit(!isEdit)
-    }
+  const handleEdit = () => {
+    setEdit(!isEdit);
+  };
 
-    return (
-        <div className="teacher-dashboard-container">
-            {isEdit ? (
-                <div className="teacher-profile">
-                    <img src={teacherProfile.image} alt='teacher profile'/>
-                    <div className="name-email">
-                        <div><b>{teacherProfile.name}</b></div>
-                        <div>{teacherProfile.email}</div>
-                    </div>
-                    <span className="edit-teacher-profile"><u onClick={handleEdit}> Edit Profile </u></span>
-                </div>
-            ) : (
-                    <div className='teacher-profile'>
-                        <div className='teacher-profile-edit'>
-                            <img src={teacherProfile.image} alt='student' />
-                            <form>
-                                <p>Name<span>*</span></p>
-                                <input type="text" placeholder="John Doe" />
-                                <br />
-                                <br />
-                                <p>Email<span>*</span></p>
-                                <input type="email" placeholder="john@gmail.com" />
-                                <br />
-                                <br />
-                                <button onClick={handleEdit}>Save Changes</button>
-                            </form>
-                        </div>
-                    </div>
-                )}
-
-            <div className="courses-container">
-                <div className="courses-header">
-                    <h5><b>Courses</b></h5>
-                    <Link to="/teacher-new-course"><button>New Course</button></Link>
-                </div>
-                <hr />
-                {courseCardJson ? courseCardJson.map((item, index) => (
-                    <CourseCard
-                        key={index}
-                        image={item.image}
-                        title={item.title}
-                        numOfVideos={item.videoNums}
-                        numOfLesson={item.lessonNums}
-                        enrolledStudents={item.studentEnrolled}
-                    />
-                )) : <div>Loading...</div>}
-
+  return (
+    <div className="teacher-dashboard-container">
+      {isEdit ? (
+        <div className="teacher-profile">
+          <img src={teacherProfile.image} alt="teacher profile" />
+          <div className="name-email">
+            <div>
+              <b>{teacherProfile.name}</b>
             </div>
+            <div>{teacherProfile.email}</div>
+          </div>
+          <span className="edit-teacher-profile">
+            <u onClick={handleEdit}> Edit Profile </u>
+          </span>
         </div>
-    );
+      ) : (
+        <div className="teacher-profile">
+          <div className="teacher-profile-edit">
+            <img src={teacherProfile.image} alt="student" />
+            <form>
+              <p>
+                Name<span>*</span>
+              </p>
+              <input type="text" placeholder="John Doe" />
+              <br />
+              <br />
+              <p>
+                Email<span>*</span>
+              </p>
+              <input type="email" placeholder="john@gmail.com" />
+              <br />
+              <br />
+              <button onClick={handleEdit}>Save Changes</button>
+            </form>
+          </div>
+        </div>
+      )}
+
+      <div className="courses-container">
+        <div className="courses-header">
+          <h5>
+            <b>Courses</b>
+          </h5>
+          <Link to="/teacher-new-course">
+            <button>New Course</button>
+          </Link>
+        </div>
+        <hr />
+        {courseCardJson ? (
+          courseCardJson.map((item, index) => (
+            <CourseCard
+              key={index}
+              image={item.image}
+              title={item.title}
+              numOfVideos={item.videoNums}
+              numOfLesson={item.lessonNums}
+              enrolledStudents={item.studentEnrolled}
+            />
+          ))
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default TeacherDashboard;
