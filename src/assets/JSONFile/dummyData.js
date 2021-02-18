dff991bb57c382a18c84045210e9c8c2f839145e
Abdurrohim_ethan <abdurrohim48@gmail.com>
2021-02-18 22:26:49 +0700

    fixing dropdown functionality on page assemnt.js teacher role

--- src/assets/JSONFile/dummyData.js
+++ src/assets/JSONFile/dummyData.js
@@ -1,7 +1,7 @@
 import Rectangle5 from "../../assets/Rectangle5.png";
-import profilePic from "../../assets/studentpicture.png"
-import reactCourse from "../../assets/reactCourse.png"
-import imgCourse from "../course-image.png"
+import profilePic from "../../assets/studentpicture.png";
+import reactCourse from "../../assets/reactCourse.png";
+import imgCourse from "../course-image.png";
 
 export const staticImage = [
   {
@@ -86,22 +86,22 @@ export const staticImage = [
   },
 ];
 
-export const buttonMaterials=[
-  {name: "BUSSINESS"},
-  {name: "TECHNOLOGY"},
-  {name: "BUSSINESS"},
-  {name: "GRAPHIC DESAIN"},
-  {name: "BUSSINESS"},
-  {name: "TECHNOLOGY"},
-  {name: "BUSSINESS"},
-  {name: "GRAPHIC DESAIN"},
-  {name: "BUSSINESS"},
-  {name: "TECHNOLOGY"},
-  {name: "BUSSINESS"},
-  {name: "GRAPHIC DESAIN"},
-]
+export const buttonMaterials = [
+  { name: "BUSSINESS" },
+  { name: "TECHNOLOGY" },
+  { name: "BUSSINESS" },
+  { name: "GRAPHIC DESAIN" },
+  { name: "BUSSINESS" },
+  { name: "TECHNOLOGY" },
+  { name: "BUSSINESS" },
+  { name: "GRAPHIC DESAIN" },
+  { name: "BUSSINESS" },
+  { name: "TECHNOLOGY" },
+  { name: "BUSSINESS" },
+  { name: "GRAPHIC DESAIN" },
+];
 
-export const cardMaterial=[
+export const cardMaterial = [
   {
     image: Rectangle5,
     text:
@@ -141,14 +141,14 @@ export const cardMaterial=[
     video_numbers: "15 Videos",
     material_numbers: "3 Learning Material",
     footer: "Business",
-  }
-]
+  },
+];
 
 export const teacherProfile = {
   name: "John Doe",
   email: "john.doe@gmail.com",
-  image: profilePic
-}
+  image: profilePic,
+};
 
 export const courseCardJson = [
   {
@@ -156,86 +156,98 @@ export const courseCardJson = [
     title: "React Crash Course I",
     videoNums: "10 Videos",
     lessonNums: "11 Lesson Materials",
-    studentEnrolled: "120 Students enrolled"
+    studentEnrolled: "120 Students enrolled",
   },
   {
     image: reactCourse,
     title: "React Crash Course II",
     videoNums: "20 Videos",
     lessonNums: "10 Lesson Materials",
-    studentEnrolled: "205 Students enrolled"
+    studentEnrolled: "205 Students enrolled",
   },
   {
     image: reactCourse,
     title: "React Crash Course III",
     videoNums: "19 Videos",
     lessonNums: "13 Lesson Materials",
-    studentEnrolled: "120 Students enrolled"
-  }
-]
+    studentEnrolled: "120 Students enrolled",
+  },
+];
 
 export const studentAssessment = [
   {
     noQuestion: 1,
-    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis pretium cursus. Nullam tristique laoreet aliquam. Maecenas tempor lacinia magna?',
-    choiceOne: 'sit amet auctor nulla aliquet eget',
-    choiceTwo: 'sit amet auctor nulla aliquet eget',
-    choiceThree: 'sit amet auctor nulla aliquet eget',
-    choiceFour: 'sit amet auctor nulla aliquet eget',
-    choiceFive: 'sit amet auctor nulla aliquet eget',
-    remark: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
-    isCorrect: true
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis pretium cursus. Nullam tristique laoreet aliquam. Maecenas tempor lacinia magna?",
+    choiceOne: "sit amet auctor nulla aliquet eget",
+    choiceTwo: "sit amet auctor nulla aliquet eget",
+    choiceThree: "sit amet auctor nulla aliquet eget",
+    choiceFour: "sit amet auctor nulla aliquet eget",
+    choiceFive: "sit amet auctor nulla aliquet eget",
+    remark:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.",
+    isCorrect: true,
+    isChosen: false,
   },
   {
     noQuestion: 2,
-    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis pretium cursus. Nullam tristique laoreet aliquam. Maecenas tempor lacinia magna?',
-    choiceOne: 'sit amet auctor nulla aliquet eget',
-    choiceTwo: 'sit amet auctor nulla aliquet eget',
-    choiceThree: 'sit amet auctor nulla aliquet eget',
-    choiceFour: 'sit amet auctor nulla aliquet eget',
-    choiceFive: 'sit amet auctor nulla aliquet eget',
-    remark: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
-    isCorrect: true
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis pretium cursus. Nullam tristique laoreet aliquam. Maecenas tempor lacinia magna?",
+    choiceOne: "sit amet auctor nulla aliquet eget",
+    choiceTwo: "sit amet auctor nulla aliquet eget",
+    choiceThree: "sit amet auctor nulla aliquet eget",
+    choiceFour: "sit amet auctor nulla aliquet eget",
+    choiceFive: "sit amet auctor nulla aliquet eget",
+    remark:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.",
+    isCorrect: true,
+    isChosen: false,
   },
   {
     noQuestion: 3,
-    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis pretium cursus. Nullam tristique laoreet aliquam. Maecenas tempor lacinia magna?',
-    choiceOne: 'sit amet auctor nulla aliquet eget',
-    choiceTwo: 'sit amet auctor nulla aliquet eget',
-    choiceThree: 'sit amet auctor nulla aliquet eget',
-    choiceFour: 'sit amet auctor nulla aliquet eget',
-    choiceFive: 'sit amet auctor nulla aliquet eget',
-    remark: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
-    isCorrect: true
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis pretium cursus. Nullam tristique laoreet aliquam. Maecenas tempor lacinia magna?",
+    choiceOne: "sit amet auctor nulla aliquet eget",
+    choiceTwo: "sit amet auctor nulla aliquet eget",
+    choiceThree: "sit amet auctor nulla aliquet eget",
+    choiceFour: "sit amet auctor nulla aliquet eget",
+    choiceFive: "sit amet auctor nulla aliquet eget",
+    remark:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.",
+    isCorrect: true,
+    isChosen: false,
   },
   {
     noQuestion: 4,
-    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis pretium cursus. Nullam tristique laoreet aliquam. Maecenas tempor lacinia magna?',
-    choiceOne: 'sit amet auctor nulla aliquet eget',
-    choiceTwo: 'sit amet auctor nulla aliquet eget',
-    choiceThree: 'sit amet auctor nulla aliquet eget',
-    choiceFour: 'sit amet auctor nulla aliquet eget',
-    choiceFive: 'sit amet auctor nulla aliquet eget',
-    remark: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
-    isCorrect: false
-  }
-]
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mollis pretium cursus. Nullam tristique laoreet aliquam. Maecenas tempor lacinia magna?",
+    choiceOne: "sit amet auctor nulla aliquet eget",
+    choiceTwo: "sit amet auctor nulla aliquet eget",
+    choiceThree: "sit amet auctor nulla aliquet eget",
+    choiceFour: "sit amet auctor nulla aliquet eget",
+    choiceFive: "sit amet auctor nulla aliquet eget",
+    remark:
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.",
+    isCorrect: false,
+    isChosen: false,
+  },
+];
 
 export const teacherAssessment = [
   {
     noQuestion: null,
-    question: '',
-    optionOne: '',
-    optionTwo: '',
-    optionThree: '',
-    optionFour: '',
-    remark: '',
-  }
-]
+    question: "",
+    optionOne: "",
+    optionTwo: "",
+    optionThree: "",
+    optionFour: "",
+    remark: "",
+  },
+];
 
 export const studentEnroll = [
   {
-    name: 'Justin Junaedi',
+    name: "Justin Junaedi",
     isCompleted: false,
     isActive: true,
     isPending: false,
@@ -244,7 +256,7 @@ export const studentEnroll = [
     score: null,
   },
   {
-    name: 'Travis Sucipto',
+    name: "Travis Sucipto",
     isCompleted: true,
     isActive: false,
     isPending: false,
@@ -253,7 +265,7 @@ export const studentEnroll = [
     score: 73,
   },
   {
-    name: 'Travis Sucipto',
+    name: "Travis Sucipto",
     isCompleted: true,
     isActive: false,
     isPending: false,
@@ -262,7 +274,7 @@ export const studentEnroll = [
     score: 73,
   },
   {
-    name: 'Asahi Muhammad',
+    name: "Asahi Muhammad",
     isCompleted: false,
     isActive: false,
     isPending: true,
@@ -271,20 +283,20 @@ export const studentEnroll = [
     score: null,
   },
   {
-    name: 'Asahi Muhammad',
+    name: "Asahi Muhammad",
     isCompleted: false,
     isActive: false,
     isPending: true,
     noQuestion: null,
     totalQuestion: null,
     score: null,
-  }
-]
+  },
+];
 
 export const studentCourses = [
   {
-    title: 'React Crash Course',
-    writer: 'Travery Media',
+    title: "React Crash Course",
+    writer: "Travery Media",
     totalCourse: 8,
     courseDone: 2,
     image: imgCourse,
@@ -295,20 +307,19 @@ export const studentCourses = [
     rightAnswer: null,
     totalQuestion: null,
     id: 1,
-    lesson: 
-      {
-        1: 'What is React Course?',
-        2: 'How is React Course?',
-        3: 'Why is React Course?',
-        4: 'Loreum Ipsum',
-        5: 'Loreum Ipsum',
-        6: 'Loreum Ipsum',
-        7: 'Loreum Ipsum',
-      }
+    lesson: {
+      1: "What is React Course?",
+      2: "How is React Course?",
+      3: "Why is React Course?",
+      4: "Loreum Ipsum",
+      5: "Loreum Ipsum",
+      6: "Loreum Ipsum",
+      7: "Loreum Ipsum",
+    },
   },
   {
-    title: 'Build Web Game Using React',
-    writer: 'Travery Media',
+    title: "Build Web Game Using React",
+    writer: "Travery Media",
     totalCourse: 15,
     courseDone: 8,
     image: imgCourse,
@@ -318,34 +329,34 @@ export const studentCourses = [
     completeDate: null,
     rightAnswer: null,
     totalQuestion: null,
-    id: 2
+    id: 2,
   },
   {
-    title: 'Create Cinematic Music Video',
-    writer: 'John Doe',
+    title: "Create Cinematic Music Video",
+    writer: "John Doe",
     totalCourse: 15,
     courseDone: 8,
     image: imgCourse,
     isCompleted: true,
     isPending: false,
     isActive: false,
-    completeDate: '28 January 2021 08:00pm',
+    completeDate: "28 January 2021 08:00pm",
     rightAnswer: 11,
     totalQuestion: 15,
-    id: 3
+    id: 3,
   },
   {
-    title: 'Create Cinematic Music Video',
-    writer: 'John Doe',
+    title: "Create Cinematic Music Video",
+    writer: "John Doe",
     totalCourse: 15,
     courseDone: 8,
     image: imgCourse,
     isCompleted: true,
     isPending: false,
     isActive: false,
-    completeDate: '28 January 2021 08:00pm',
+    completeDate: "28 January 2021 08:00pm",
     rightAnswer: 11,
     totalQuestion: 15,
-    id: 4
+    id: 4,
   },
-]
+];
--- src/pages/TeacherNewCourse/Tab/DashBoardCourse/CourseAssesment.js
+++ src/pages/TeacherNewCourse/Tab/DashBoardCourse/CourseAssesment.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { studentAssessment as assessment } from "../../../../assets/JSONFile/dummyData";
+import imgDropdown from "../../../../assets/dropdownsymbol.png";
+
 function CourseAssesment() {
+  const [isPicked, setPicked] = useState({
+    data: [...assessment],
+  });
+  function handleDropDown(index) {
+    isPicked.data[index].isChosen
+      ? (isPicked.data[index].isChosen = false)
+      : (isPicked.data[index].isChosen = true);
+    setPicked({ ...isPicked });
+  }
+  function handleDropDownActive(index) {
+    if (isPicked.data[index].isChosen) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   return (
     <div>
       <div className="teacher-dashboard-list">
@@ -11,6 +31,53 @@ function CourseAssesment() {
           <p>Assessment</p>
         </Link>
         <p className="open">Students</p>
+      </div>
+      <div className="save-question-box">
+        {assessment.map((item, index) => (
+          <div className="questions-answer-box-save" key={index}>
+            <div className="question-dropdown">
+              <p>
+                {item.noQuestion}. {item.question}
+              </p>
+              <p>
+                <img
+                  src={imgDropdown}
+                  alt="symbol"
+                  onClick={() => {
+                    handleDropDown(index);
+                  }}
+                />
+              </p>
+            </div>
+            {handleDropDownActive(index) && (
+              <div>
+                <label className="container">
+                  <p className="answer">Answer</p>
+                  <input type="radio" name="radio" />
+                  <span>{item.choiceOne}</span>
+                </label>
+                <label className="container">
+                  <input type="radio" name="radio" />
+                  <span>{item.choiceTwo}</span>
+                </label>
+                <label className="container">
+                  <input type="radio" name="radio" />
+                  <span>{item.choiceThree}</span>
+                </label>
+                <label className="container">
+                  <input type="radio" name="radio" />
+                  <span>{item.choiceFour}</span>
+                </label>
+                <label className="container">
+                  <input type="radio" name="radio" />
+                  <span>{item.choiceFive}</span>
+                </label>
+              </div>
+            )}
+            <br />
+            <br />
+          </div>
+        ))}
       </div>
     </div>
   );
