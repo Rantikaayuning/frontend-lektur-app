7897c2fec2cb2d52eabca01f82248206216c3ebb
yulisartika <yulisrtk@gmail.com>
2021-02-23 09:11:57 +0700

    added correct answers

--- src/pages/StudentAssessment/index.js
+++ src/pages/StudentAssessment/index.js
@@ -1,63 +1,78 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from "react-redux"
-import { getQuestions } from '../../redux/actions/AssessmentAction'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getQuestions } from "../../redux/actions/AssessmentAction";
 
 const StudentAssessment = () => {
-    // const {id} = useParams();
-    const dispatch = useDispatch();
+  // const {id} = useParams();
+  const dispatch = useDispatch();
 
-    const assessment = useSelector(state => state.assessment.assessment)
+  const assessment = useSelector((state) => state.assessment.assessment);
 
-    useEffect(() => {
-        dispatch(getQuestions());
-      }, [dispatch]);
+  useEffect(() => {
+    dispatch(getQuestions());
+  }, [dispatch]);
 
-    // console.log(assessment)
+  // console.log(assessment)
 
-    return (
-        <>
-            <div className='student-assessment'>
+  return (
+    <>
+      <div className="student-assessment">
         {assessment !== null ? (
-            <>
-                <div className="assessment-title">
-                    <div>
-                        <span className="bread-crumb">Create Cinematic Music Video</span> /  {" "}
-                        <span className="link">Final Assessment</span>
-                    </div>
-                    <div className="final-assessment-title">Final Assessment</div>
-                </div>
-                <div className='student-assessment-box'>
-                    <h4>{assessment.length} Questions</h4>
-                    <hr class="solid"></hr>
-                    {assessment.map((item, index) => (
-                        <div className='assessment-questions' key={index}>
-                            <p>{item.number}. {item.question}</p>
-                            <p>Answer</p>
-                            <>
-                            {item.options.map((item, id) => (
-                                <label class="container">
-                                    <input type="radio" name="radio" value={item.value}/>{' '}
-                                    <span>{item.text}</span>
-                                </label>
-                            ))}
-                            </>
-                        </div>
-                            ))}
-                    </div>
-                </>
-        ) : (
-            <div id="loader"></div>
-        )}
-                
-                <div className='submit-assessment'>
-                    <Link to='/assessment-result'>
-                        <p><button>Submit Assessment</button></p>
-                    </Link>
-                </div>
+          <>
+            <div className="assessment-title">
+              <div>
+                <span className="bread-crumb">
+                  Create Cinematic Music Video
+                </span>{" "}
+                / <span className="link">Final Assessment</span>
+              </div>
+              <div className="final-assessment-title">Final Assessment</div>
             </div>
-        </>
-    )
-}
+            <div className="student-assessment-box">
+              <h4>{assessment.length} Questions</h4>
+              <hr class="solid"></hr>
+              <form onSubmit={""}>
+                {assessment.map((item, index) => (
+                  <div className="assessment-questions" key={index}>
+                    <p>
+                      {item.number}. {item.question}
+                    </p>
+                    <p>Answer</p>
+                    <>
+                      {item.options.map((opt, index) => (
+                        <label class="container">
+                          <input
+                            type="radio"
+                            name={index}
+                            id={opt._id}
+                            value={opt.value}
+                            checked={item.answer === opt.value} // correct value
+                            onChange={""}
+                          />{" "}
+                          <span>{opt.text}</span>
+                        </label>
+                      ))}
+                    </>
+                  </div>
+                ))}
+              </form>
+            </div>
+          </>
+        ) : (
+          <div id="loader"></div>
+        )}
+
+        <div className="submit-assessment">
+          <Link to="/assessment-result">
+            <p>
+              <button>Submit Assessment</button>
+            </p>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default StudentAssessment;
--- src/redux/reducers/AssessmentReducer.js
+++ src/redux/reducers/AssessmentReducer.js
@@ -1,21 +1,20 @@
-import { GET_QUESTIONS } from '../types/AssessmentTypes'
+import { GET_QUESTIONS } from "../types/AssessmentTypes";
 
 const initialState = {
-    assessment: null,
-  };
-  
-  const assessmentReducer = (state = initialState, action) => {
-    const { type, payload } = action;
-    switch (type) {
-      case GET_QUESTIONS:
-        return {
-          ...state,
-          assessment: payload,
-        };
-      default :
-        return state;
-    }
-  };
-  
-  export default assessmentReducer;
-  
+  assessment: null,
+};
+
+const assessmentReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case GET_QUESTIONS:
+      return {
+        ...state,
+        assessment: payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default assessmentReducer;
