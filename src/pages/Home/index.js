3214ff471188f3d28d34d13dfa8a494536f7215c
yulisartika <yulisrtk@gmail.com>
2021-03-02 11:07:23 +0700

    remove now word on register for testing

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,30 +3,26 @@ import bgcontent from "../../assets/rectangle55.png";
 import { Link } from "react-router-dom";
 import Content from "../../components/ContentCard/index";
 import Jumbo from "../../components/Header/Jumbotron";
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux";
 
 const Home = () => {
-  const {searchCourse } = useSelector(state => state.courses)
+  const { searchCourse } = useSelector((state) => state.courses);
 
   return (
     <>
-      {searchCourse !== '' ? (
-        <div></div>
-      ) : (
-        <Jumbo />
-      )}
-        <Content />
-        <div className="content-register">
-          <img src={bgcontent} alt="background content" />
-          <div className="register-button">
-            <h1>
-              Create Your <span className="own-class">Own Class</span>
-            </h1>
-            <Link to="/register">
-              <button>Register now</button>
-            </Link>
-          </div>
-        </div>        
+      {searchCourse !== "" ? <div></div> : <Jumbo />}
+      <Content />
+      <div className="content-register">
+        <img src={bgcontent} alt="background content" />
+        <div className="register-button">
+          <h1>
+            Create Your <span className="own-class">Own Class</span>
+          </h1>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
