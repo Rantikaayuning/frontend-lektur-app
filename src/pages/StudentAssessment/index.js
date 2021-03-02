72c97ec029a03e1adf0e2180ffd55fe073fa3bc3
yulisartika <yulisrtk@gmail.com>
2021-03-02 10:50:03 +0700

    try fixing radio buttons on student assessment

--- src/pages/StudentAssessment/index.js
+++ src/pages/StudentAssessment/index.js
@@ -1,71 +1,100 @@
-import React, { useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom'
-import { useDispatch, useSelector } from "react-redux"
-import { getQuestions } from '../../redux/actions/AssessmentAction'
-import { getCourseDetail } from '../../redux/actions/CoursesAction';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getQuestions } from "../../redux/actions/AssessmentAction";
+import { getCourseDetail } from "../../redux/actions/CoursesAction";
 
 const StudentAssessment = () => {
-    // const {id} = useParams();
-    const dispatch = useDispatch();
-    const {id} = useParams()
+  const dispatch = useDispatch();
+  const { id } = useParams();
 
-    const {assessment} = useSelector(state => state.assessment)
-    const {courseDetail} = useSelector(state => state.courses)
+  const [selected, setSelected] = useState(null);
 
-    useEffect(() => {
-        dispatch(getQuestions(id));
-        dispatch(getCourseDetail(id))
-      }, [dispatch, id]);
+  const { assessment } = useSelector((state) => state.assessment);
+  const { courseDetail } = useSelector((state) => state.courses);
 
-    console.log(assessment)
+  useEffect(() => {
+    dispatch(getQuestions(id));
+    dispatch(getCourseDetail(id));
+  }, [dispatch, id]);
 
-    return (
-        <>
-        {courseDetail !== null && assessment !== null ? (
-            <div className='student-assessment'>
-                <div className="assessment-title">
-                    {courseDetail === null ? (
-                    <div>
-                        <span className="link">Final Assessment</span>
-                    </div>
-                    ) : (
-                    <div>
-                        <span className="bread-crumb">{courseDetail.course.title}</span> /  {" "}
-                        <span className="link">Final Assessment</span>
-                    </div>
-                    )}
-                    <div className="final-assessment-title">Final Assessment</div>
-                </div>
-                <div className='student-assessment-box'>
-                    <h4>{assessment.length} Questions</h4>
-                    <hr class="solid"></hr>
-                    {assessment.map((item, index) => (
-                    <div className='assessment-questions' key={index}>
-                        <p>{item.number}. {item.question}</p>
-                        <p>Answer</p>
-                        <>
-                        {item.options.map((item, id) => (
-                            <label class="container">
-                                <input type="radio" name="radio" value={item.value}/>{' '}
-                                <span>{item.text}</span>
-                            </label>
-                        ))}
-                        </>
-                    </div>
-                        ))}
-                </div>
-                <div className='submit-assessment'>
-                    <Link to={`/assessment/result/${assessment[0].courseId}`}>
-                        <p><button>Submit Assessment</button></p>
-                    </Link>
-                </div>
-            </div>
-        ) : (
-            <div id="loader"></div>
-        )}
-                
-        </>
-    )
-}
+  const correctAnswer = assessment.filter((item) => item.answer);
+  console.log(correctAnswer);
+
+  const onValueChange = (e) => {
+    setSelected(e.target.value);
+    // console.log(selected);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    // then post something
+    console.log(selected);
+  };
+
+  return (
+    <>
+      {courseDetail !== null && assessment !== null ? (
+        <div className="student-assessment">
+          <div className="assessment-title">
+            {courseDetail === null ? (
+              <div>
+                <span className="link">Final Assessment</span>
+              </div>
+            ) : (
+              <div>
+                <span className="bread-crumb">{courseDetail.course.title}</span>{" "}
+                / <span className="link">Final Assessment</span>
+              </div>
+            )}
+            <div className="final-assessment-title">Final Assessment</div>
+          </div>
+          <div className="student-assessment-box">
+            <h4>{assessment.length} Questions</h4>
+            <hr class="solid"></hr>
+            {assessment.map((item, index) => (
+              <div className="assessment-questions" key={index}>
+                <p>
+                  {item.number}. {item.question}
+                </p>
+                <p>Answer</p>
+                <>
+                  {item.options.map((item_, index) => (
+                    <label class="container">
+                      <input
+                        type="radio"
+                        name={`${index + 1}`}
+                        value={item_.value}
+                        checked={selected === item_.value}
+                        onChange={onValueChange}
+                      />
+                      <span> {item_.text}</span>
+                    </label>
+                  ))}
+                  <div>
+                    Selected option is : {selected}{" "}
+                    {/* <span>
+                      {item.answer === selected ? " correct" : " wrong"}
+                    </span> */}
+                  </div>
+                </>
+              </div>
+            ))}
+          </div>
+          <div className="submit-assessment">
+            <Link to={`/assessment/result/${assessment[0].courseId}`}>
+              <p>
+                <button onClick={onFormSubmit}>Submit Assessment</button>
+                {/* after submit, get the score, then post the score */}
+              </p>
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <div id="loader"></div>
+      )}
+    </>
+  );
+};
 
 export default StudentAssessment;
