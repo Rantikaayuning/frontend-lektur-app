ce38977e27e5b3f5e00b7c203056c4d2a6400149
yulisartika <yulisrtk@gmail.com>
2021-03-08 16:29:01 +0700

    fixing edit user profile for student and teacher

--- src/pages/TeacherDashboard/index.js
+++ src/pages/TeacherDashboard/index.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { connect } from "react-redux";
 import Cookies from "js-cookie";
-// import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import { teacherProfile } from "../../assets/JSONFile/dummyData";
 import CourseCard from "./CourseCard";
@@ -14,29 +13,31 @@ import {
 import { getTeacherCourses } from "../../redux/actions/CoursesAction";
 import defaultImg from "../../assets/defaultLektur.png";
 
-function TeacherDashboard(props) {
+function TeacherDashboard() {
+  const dispatch = useDispatch();
+
   const [isEdit, setEdit] = useState(true);
+  const [forgetAlert, setForgetAlert] = useState("");
+
   const [fullname, setFullname] = useState("");
   const [email, setEmail] = useState("");
 
-  const handleEdit = () => {
+  const userProfile = useSelector((state) => state.users.userProfile);
+  const teacherCourses = useSelector((state) => state.courses.teacherCourses);
+  const token = useSelector((state) => state.users.token);
+
+  const handleEdit = async () => {
     setEdit(!isEdit);
   };
 
   useEffect(() => {
-    props.token ? props.getUserProfile(props.token) : props.getUserProfile();
-    props.token
-      ? props.getTeacherCourses(props.token)
-      : props.getTeacherCourses();
-  }, []);
-
-  // console.log(props.userProfile);
-  // console.log(props.teacherCourses);
+    token ? dispatch(getUserProfile(token)) : dispatch(getUserProfile());
+    token ? dispatch(getTeacherCourses(token)) : dispatch(getTeacherCourses());
+  }, [dispatch]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    props
-      .updateUserProfile(fullname, email)
+    handleEdit(dispatch(updateUserProfile(fullname, email)))
       .then(() =>
         alert(`Hi ${fullname} Please do login back to get your changes`)
       )
@@ -48,14 +49,14 @@ function TeacherDashboard(props) {
     <div className="teacher-dashboard-container">
       {isEdit ? (
         <div>
-          {props.userProfile ? (
+          {userProfile ? (
             <div className="teacher-profile">
               <img src={teacherProfile.image} alt="teacher profile" />
               <div className="name-email">
                 <div>
-                  <b>{props.userProfile.fullname}</b>
+                  <b>{userProfile.fullname}</b>
                 </div>
-                <div>{props.userProfile.email}</div>
+                <div>{userProfile.email}</div>
               </div>
               <span className="edit-teacher-profile">
                 <u onClick={handleEdit}> Edit Profile </u>
@@ -69,13 +70,20 @@ function TeacherDashboard(props) {
         <div className="teacher-profile">
           <div className="teacher-profile-edit">
             <img src={teacherProfile.image} alt="student" />
-            <form onSubmit={handleSubmit}>
+            <form
+              onSubmit={(e) =>
+                fullname && email
+                  ? handleSubmit(e)
+                  : // : setForgetAlert("*fill in the form correctly")
+                    alert("Please fill in the form correctly")
+              }
+            >
               <p>
                 Name<span>*</span>
               </p>
               <input
                 type="text"
-                placeholder="John Doe"
+                placeholder={userProfile.fullname}
                 onChange={(e) => setFullname(e.target.value)}
                 value={fullname}
               />
@@ -86,19 +94,20 @@ function TeacherDashboard(props) {
               </p>
               <input
                 type="email"
-                placeholder="john@gmail.com"
+                placeholder={userProfile.email}
                 onChange={(e) => setEmail(e.target.value)}
                 value={email}
               />
               <br />
               <br />
+              {/* <p>{forgetAlert}</p> */}
               <button>Save Changes</button>
             </form>
           </div>
         </div>
       )}
 
-      {props.teacherCourses !== 0 ? (
+      {teacherCourses !== 0 ? (
         <div className="courses-container">
           <div className="courses-header">
             <h5>
@@ -110,7 +119,7 @@ function TeacherDashboard(props) {
           </div>
           <div className="card-teacher-container overflow-auto">
             <hr />
-            {props.teacherCourses.map((item, index) => (
+            {teacherCourses.map((item, index) => (
               <CourseCard
                 key={index}
                 image={item.image === null ? defaultImg : item.image}
@@ -130,17 +139,4 @@ function TeacherDashboard(props) {
   );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    userProfile: state.users.userProfile,
-    updateUser: state.users.updateUser,
-    teacherCourses: state.courses.teacherCourses,
-    token: state.users.token,
-  };
-};
-
-export default connect(mapStateToProps, {
-  getUserProfile,
-  updateUserProfile,
-  getTeacherCourses,
-})(TeacherDashboard);
+export default TeacherDashboard;
